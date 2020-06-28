cfbb91ce77236986434129a829f3f5254007f009
cheeseBG <51084152+JangJungIk@users.noreply.github.com>
2020-06-29 00:49:23 +0900

    Detail Activity
    Activity, Toobar 추가

--- app/src/main/java/com/gachon/smartedu/activity/LectureDetailActivity.java
+++ app/src/main/java/com/gachon/smartedu/activity/LectureDetailActivity.java
@@ -6,10 +6,13 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.gachon.smartedu.R;
 
 public class LectureDetailActivity extends AppCompatActivity {
+    LinearLayout participant, post, assignment, announce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,47 @@ public class LectureDetailActivity extends AppCompatActivity {
 
         String name = intent.getExtras().getString("LectureName");
         // String LID = intetn.getExtras().getString("LID");
+
+        participant = findViewById(R.id.participant);
+        post = findViewById(R.id.post);
+        assignment = findViewById(R.id.assignment);
+        announce = findViewById(R.id.announce);
+
+        participant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LectureDetailActivity.this, ParticipantActivity.class);
+
+                startActivity(intent);
+                }
+            });
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LectureDetailActivity.this, PostActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        assignment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LectureDetailActivity.this, AssignmentActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        announce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LectureDetailActivity.this, AnnounceActivity.class);
+
+                startActivity(intent);
+            }
+        });
 
         // 툴바에 뒤로가기 버튼 만들기
         Toolbar toolBar = findViewById(R.id.lecture_detail_toolbar);
